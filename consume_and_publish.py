c6480b4ae172abb2673563fc24d132e3b680840c
jemunos <jmunos@munoscc.com>
2018-06-04 20:55:32 +0000

    This is working, however it will time out and has no error checking or redundancy.

--- consume_and_publish.py
+++ consume_and_publish.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pika
+import pprint
 import requests
 from config import RMQ_PORT, RMQ_SERVER, RMQ_QUEUE, API_SERVER, EVENT_STREAM_NAME, API_KEY, CLIENT_ID
 
@@ -14,23 +15,27 @@ local_channel = local_connection.channel()
 local_channel.queue_declare(queue=RMQ_QUEUE, durable=True)
 
 # Setup a session and its authentication
-session = requests.Session()
-session.auth = (CLIENT_ID, API_KEY)
+amp_session = requests.Session()
+amp_session.auth = (CLIENT_ID, API_KEY)
 
 # Get a list of all event streams on the server
-event_streams = session.get(API_SERVER).json()['data']
+event_streams = amp_session.get(API_SERVER).json()['data']
 
-# Declare a dictionary to the event stream ID
+# Declare a dictionary to store the event stream ID
 event_stream = {}
 
 # Iterate though the available event_streams, identify the event stream that matches EVENT_STREAM_NAME
-# Store the event stream data in e
-for e in event_streams:
-	if e['name'] is EVENT_STREAM_NAME:
-		event_stream = e
+# Store the event stream data in event_stream
+for k in event_streams:
+    if k['name'] == EVENT_STREAM_NAME:
+        event_stream = k
+
 
 # Build the URL that our listener will monitor on the AMP cloud and store it as queue_url
-cloud_queue_url = pika.URLParameters('amqps://{user_name}:{password}@{host}:{port}'.format(**e['amqp_credentials']))
+cloud_queue_url = pika.URLParameters('amqps://{user_name}:{password}@{host}:{port}'
+                                     .format(**event_stream['amqp_credentials']))
+pprint.pprint(event_stream['amqp_credentials']['password'])
+
 
 # Configure the connection by passing it the queue URL
 cloud_connection = pika.BlockingConnection(cloud_queue_url)
@@ -38,19 +43,22 @@ cloud_connection = pika.BlockingConnection(cloud_queue_url)
 # Open a channel using the connection
 cloud_channel = cloud_connection.channel()
 
-def callback(ch, method, properties, body):
-    print(" [x] Received meth:\t%r" % method)
-    print(" [x] Received prop:\t%r" % properties)
-    print(" [x] Received body:\t%r" % body)
+def on_message(channel, method_frame, header_frame, body):
+       local_channel.basic_publish(exchange='',
+                                   routing_key=RMQ_QUEUE,
+                                   body=body,
+                                   properties=pika.BasicProperties(
+                                       delivery_mode=2,  # make message persistent
+                                   ))
 
+cloud_channel.basic_consume(on_message, event_stream['amqp_credentials']['queue_name'], no_ack=True)
+try:
+    cloud_channel.start_consuming()
+except KeyboardInterrupt:
+    cloud_channel.stop_consuming()
+cloud_connection.close()
 
-cloud_channel.basic_consume(callback, e, no_ack=True)
 
 print(" [*] Connecting to:\t%r" % cloud_queue_url)
 print(" [*] Waiting for messages. To exit press CTRL+C")
 cloud_channel.start_consuming()
-
-
-
-
-
